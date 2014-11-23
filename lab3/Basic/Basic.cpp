30cad3de8fdeb6252238e1f7d9a3703c9991cd4a
NoteBookie <jianian101@gmail.com>
2014-11-23 21:19:27 +0800

    homework8 PartA

--- lab3/Basic/Basic.cpp
+++ lab3/Basic/Basic.cpp
@@ -5,6 +5,59 @@
 ** Created Time: 2014年11月11日 星期二 23时10分16秒
 ** ----------------------------------------------------------------------
 */
-#include<iostream>
+#include <iostream>
+#include "TokenScanner.h"
+#define CODE 1
+#define COMMANDS 2
 using namespace std;
 
+class Interpreter{
+private:
+	int status;
+	map of values;
+	map of codes;
+public:
+	Interpreter();
+	void init(stdin){
+		int number;
+		string commands;
+		if(cin >> number) {
+			status = CODE;
+			string exp;
+			getline(cin,exp);
+			insert into a map.
+		}else{
+			status = COMMANDS;
+			getline(cin,commands);
+			if(commands == "RUN") run();
+			else if(commands == "CLEAR") clear();
+			else if(commands == "QUIT") quit();
+			else if(commands == "LIST") list();
+			else if(commands == "HELP") help();
+		}
+	}
+	void quit(){
+		exit(0);
+	}
+	void list(){
+		print codes;
+	}
+	void help(){
+		print help;
+	}
+	void run(){
+		execute();	
+	}
+	void execute(){
+		get codes;
+		if let : then pushback name and val into a map;
+		if print : then find the val of that name;put it on stdout;
+		if input : then wait stdin input a val;
+		if end: return; 
+	}
+	
+}
+
+Expression(string){
+	change string into an int.
+}
