a835775eff7f8636e90f2e2414fcefa7d857ea75
NoteBookie <jianian101@gmail.com>
2014-11-26 17:46:04 +0800

    lab3 finish

--- homework8/starter/RedBlackTree2.h
+++ homework8/starter/RedBlackTree2.h
@@ -32,7 +32,6 @@ class RedBlackTree
      */
     explicit RedBlackTree( const Comparable & negInf )
     {
-        fatherGet = false;
         nullNode    = new RedBlackNode;
         nullNode->left = nullNode->right = nullNode;
         header      = new RedBlackNode( negInf );
@@ -41,7 +40,6 @@ class RedBlackTree
 
     RedBlackTree( const RedBlackTree & rhs )
     {
-        fatherGet = false;
         nullNode    = new RedBlackNode;
         nullNode->left = nullNode->right = nullNode;
         header      = new RedBlackNode( rhs.header->element );
@@ -51,14 +49,14 @@ class RedBlackTree
 
     ~RedBlackTree( )
     {
-        // makeEmpty( );
-        // delete nullNode;
-        // delete header;
+        makeEmpty( );
+        delete nullNode;
+        delete header;
     }
 
     const Comparable & findMin( ) const
     {
-      if( isEmpty( ) ) {}
+      //if( isEmpty( ) ) {}
 	  
 
         RedBlackNode *itr = header->right;
@@ -71,7 +69,7 @@ class RedBlackTree
 
     const Comparable & findMax( ) const
     {
-      if( isEmpty( ) ){}
+      //if( isEmpty( ) ){}
 
         RedBlackNode *itr = header->right;
 
@@ -86,7 +84,7 @@ class RedBlackTree
         nullNode->element = x;
         RedBlackNode *curr = header->right;
 
-        for( ; curr != nullNode; )
+        for( ; ; )
         {
             if( x < curr->element )
                 curr = curr->left;
@@ -95,7 +93,6 @@ class RedBlackTree
             else
                 return curr != nullNode;
         }
-        return false;
     }
 
     bool isEmpty( ) const
@@ -148,43 +145,64 @@ class RedBlackTree
         handleReorient( x );
     }
 
-    void remove( const Comparable & num )
+    void remove( const Comparable & item )
     {
-        if (!fatherGet) {
-            setParent(header);
-            fatherGet = true;
-        }
-        RedBlackNode* z = find(num);
-        RedBlackNode* y = z;
-        RedBlackNode* x = nullNode;
-        int yOriginalColor = y->color;
-        if (z->left == nullNode) {
-            x = z->right;
-            transplant(z, z->right);
-        }
-        else if (z->right == nullNode) {
-            x = z->left;
-            transplant(z, z->left);
-        }
-        else {
-            y = treeMin(z->right);
-            yOriginalColor = y->color;
-            x = y->right;
-            if (y->father == z)
-                x->father = y;
-            else {
-                transplant(y, y->right);
-                y->right = z->right;
-                y->right->father = y;
-            }
-            transplant(z, y);
-            y->left = z->left;
-            y->left->father = y;
-            y->color = z->color;
-        }
-        if (yOriginalColor == BLACK)
-            removeFixUp(x);
-        //delete x;
+		cout << "remove sth;"<<endl;
+		current = parent = grand = header;
+		nullNode->element = item;
+		current->color = RED;
+
+		while( current->element != item) {
+cout<<"loop"<<endl;
+			parent = current;
+			if( item < parent->element ) {
+				current = parent->left;
+				sibling = parent->right;
+			} else {
+				current = parent->right;
+				sibling = parent->left;
+			}
+			handleDeleteReorient( item );
+		}
+cout <<"reorient end1"<<endl;
+		if( current == nullNode ) {
+			header->right->color = BLACK;
+			return;
+		}
+
+		if( isLeaf( current ) ) {
+			if( parent->right == current )
+				parent->right = nullNode;
+			else
+				parent->left = nullNode;
+		}
+		else {
+cout <<"out loop"<<endl;
+			RedBlackNode* temp = findNextInOrder( current->element, current );
+			current->element = temp->element;
+			parent = current;
+			current = current->right;
+			sibling = parent->left;
+
+			while( current != temp ) {
+			
+				handleDeleteReorient( item );
+				parent = current;
+				current = current->left;
+				sibling = parent->right;
+			}
+
+			if( parent->right == current )
+				parent->right = current->right;
+			else
+				parent->left = current->right;
+
+			current->right->color = BLACK;
+
+		}
+cout <<"reorient end2"<<endl;
+		//Recolor the root black.
+		header->right->color = BLACK;
     }
 
     enum { RED, BLACK };
@@ -201,19 +219,17 @@ class RedBlackTree
     }
 
   private:
-    bool fatherGet;
     struct RedBlackNode
     {
         Comparable    element;
         RedBlackNode *left;
         RedBlackNode *right;
-        RedBlackNode *father;
         int           color;
 
         RedBlackNode( const Comparable & theElement = Comparable( ),
-                            RedBlackNode *lt = NULL, RedBlackNode *rt = NULL, RedBlackNode *ft = NULL,
+                            RedBlackNode *lt = NULL, RedBlackNode *rt = NULL,
                             int c = BLACK )
-          : element( theElement ), left( lt ), right( rt ), color( c ), father( NULL ) { }
+          : element( theElement ), left( lt ), right( rt ), color( c ) { }
     };
 
     RedBlackNode *header;   // The tree header (contains negInf)
@@ -224,6 +240,7 @@ class RedBlackTree
     RedBlackNode *parent;
     RedBlackNode *grand;
     RedBlackNode *great;
+	RedBlackNode *sibling;
 
         // Usual recursive stuff
     void reclaimMemory( RedBlackNode *t )
@@ -240,8 +257,8 @@ class RedBlackTree
     {
         if( t != t->left )
         {
-            cout << t->element << t->color << " ";
             printTree( t->left );
+            cout << t->element << endl;
             printTree( t->right );
         }
     }
@@ -252,7 +269,7 @@ class RedBlackTree
             return nullNode;
         else
             return new RedBlackNode( t->element, clone( t->left ),
-                                     clone( t->right ), NULL, t->color );
+                                     clone( t->right ), t->color );
     }
 
         // Red-black tree manipulations
@@ -278,6 +295,88 @@ class RedBlackTree
         header->right->color = BLACK; // Make root black
     }
 
+	void handleDeleteReorient(const Comparable & item ) {
+		cout <<"reorient"<<endl;
+		//Case if the current node is black and the sibling is red.
+		if( current->color == BLACK && sibling->color == RED ) {
+			grand = rotate( sibling->element, parent );
+			grand->color = BLACK;
+			grand->left->color = RED;
+			grand->right->color = RED;
+		}
+		//The three cases if both children of current are black.
+		else if( current->color == BLACK && sibling->color == BLACK ) {
+			if( current->left->color == BLACK
+				&& current->right->color == BLACK ) {
+				if( sibling->left->color == BLACK
+					&& sibling->right->color == BLACK ) {
+					parent->color = BLACK;
+					current->color = RED;
+					sibling->color = RED;
+				} else if( sibling->left->color == RED ) {
+					grand = rotate( sibling->left->element, parent );
+					grand->color = RED;
+					grand->left->color = BLACK;
+					grand->right->color = BLACK;
+					current->color = RED;
+				} else if( sibling->right->color == RED ) {
+					grand = rotate( sibling->right->element, parent );
+					grand->color = RED;
+					grand->left->color = BLACK;
+					grand->right->color = BLACK;
+					current->color = RED;
+				}
+			}
+		}
+		
+		//For all other cases, either current or one of its children is red,
+		//so continue to the next node.
+	}
+
+	bool isLeaf( RedBlackNode* t ) {
+		if( t != nullNode && t->left == nullNode && t->right == nullNode )
+			return true;
+		else
+			return false;
+	}
+    RedBlackNode* findMin( RedBlackNode* t ) {
+		while( t->left != nullNode && t->left != NULL )
+			t = t->left;
+		return t;
+	}
+	RedBlackNode* findNextInOrder( Comparable& item, RedBlackNode* n) {
+		cout<<"Wrong"<<endl;
+		RedBlackNode* next = n;
+		while( item > n->element && n->left != nullNode ) {
+			n = n->right;
+			if( n->right == nullNode )
+				return NULL;
+		}
+		if( item == n->element&& n->right != nullNode ){
+			return (findMin(n->right));	
+		}
+		else if( n->left != nullNode ) {
+			next = findNextInOrder( item, n->left );
+			if( next == NULL )
+				return n;
+			else
+				return next;
+		}
+		return NULL;
+	}
+
+	/*RedBlackNode* rotate( RedBlackNode* child, RedBlackNode* parent, RedBlackNode* grand ) {
+		if( grand->left == parent )
+			if( parent->left == child )
+				return grand->left = rotateWithLeftChild( parent );
+			else
+				return grand->left = rotateWithRightChild( parent );
+		else
+			if( parent->left == child )
+				return grand->right = rotateWithLeftChild( parent );
+			else
+				return grand->right = rotateWithRightChild( parent );
+	}*/
     /**
      * Internal routine that performs a single or double rotation.
      * Because the result is attached to the parent, there are four cases.
@@ -289,172 +388,30 @@ class RedBlackTree
     RedBlackNode * rotate( const Comparable & item, RedBlackNode *theParent )
     {
         if( item < theParent->element )
-        {
-            item < theParent->left->element ?
-                rotateWithLeftChild( theParent->left )  :  // LL
-                rotateWithRightChild( theParent->left ) ;  // LR
-            return theParent->left;
-        }
-        else
-        {
-            item < theParent->right->element ?
-                rotateWithLeftChild( theParent->right ) :  // RL
-                rotateWithRightChild( theParent->right );  // RR
-            return theParent->right;
-        }
+			return theParent->left = (item < theParent->left->element) ?
+				rotateWithLeftChild( theParent->left ) :
+				rotateWithRightChild( theParent->left );
+		else
+			return theParent->right = (item < theParent->right->element) ?
+				rotateWithLeftChild( theParent->right ) :
+				rotateWithRightChild( theParent->right );
+        
     }
 
-    void rotateWithLeftChild( RedBlackNode * & k2 )
+    RedBlackNode * rotateWithLeftChild( RedBlackNode *  k2 )
     {
         RedBlackNode *k1 = k2->left;
         k2->left = k1->right;
         k1->right = k2;
-        k2 = k1;
+        return k1;
     }
 
-    void rotateWithRightChild( RedBlackNode * & k1 )
+    RedBlackNode * rotateWithRightChild( RedBlackNode *  k1 )
     {
         RedBlackNode *k2 = k1->right;
         k1->right = k2->left;
         k2->left = k1;
-        k1 = k2;
-    }
-
-    // RedBlackNode* getParent(RedBlackNode* child) {
-    //     if (child == nullNode)
-    //         exit(0);
-    //     RedBlackNode* index = header;
-    //     while (index != nullNode) {
-    //         if (index->left == child || index->right == child) 
-    //             break;
-    //         else if (child->element < index->element)
-    //             index = index->left;
-    //         else
-    //             index = index->right;
-    //     }
-    //     return index;
-    // }
-
-    void transplant(RedBlackNode* u, RedBlackNode* v) {
-        if (u == header->right)
-            header->right = v;
-        else if (u == u->father->left)
-            u->father->left = v;
-        else
-            u->father->right = v;
-        v->father = u->father;
-    }
-
-    RedBlackNode* find(const Comparable& x) {
-        RedBlackNode* index = header;
-        while (index != nullNode && index->element != x) {
-            if (x < index->element)
-                index = index->left;
-            else if (x > index->element)
-                index = index->right;
-            else
-                break;
-        }
-        return index;
-    }
-
-    RedBlackNode* treeMin(RedBlackNode* x) {
-        while (x->left != nullNode)
-            x = x->left;
-        return x;
-    }
-
-    void leftRotate(RedBlackNode* x) {
-        RedBlackNode* y = x->right;
-        x->right = y->left;
-        y->left->father = x;
-        y->father = x->father;
-        if (x == x->father->left)
-            x->father->left = y;
-        else
-            x->father->right = y;
-        y->left = x;
-        x->father = y;
-    }
-
-    void rightRotate(RedBlackNode* y) {
-        RedBlackNode* x = y->left;
-        y->left = x->right;
-        x->right->father = y;
-        x->father = y->father;
-        if (y == y->father->left)
-            y->father->left = x;
-        else
-            y->father->right = x;
-        x->right = y;
-        y->father = x;
-    }
-
-    void removeFixUp(RedBlackNode* x) { 
-        while (x != header->right && x->color == BLACK) {
-            if (x == x->father->left) {
-                RedBlackNode* w = x->father->right;
-                if (w->color == RED) {
-                    w->color = BLACK;
-                    x->father->color = RED;
-                    leftRotate(x->father);
-                    w = x->father->right;
-                }
-                if (w->left->color == BLACK && w->right->color == BLACK) {
-                    w->color = RED;
-                    x = x->father;
-                }
-                else {
-                    if (w->right->color == BLACK) {
-                        w->left->color = BLACK;
-                        w->color = RED;
-                        rightRotate(w);
-                        w = x->father->right;
-                    }
-                    w->color = x->father->color;
-                    x->father->color = BLACK;
-                    w->right->color = BLACK;
-                    leftRotate(x->father);
-                    x = header->right;
-                }
-            }
-            else {
-                RedBlackNode* w = x->father->left;
-                if (w->color == RED) {
-                    w->color = BLACK;
-                    x->father->color = RED;
-                    rightRotate(x->father);
-                    w = x->father->left;
-                }
-                if (w->left->color == BLACK && w->right->color ==BLACK) {
-                    w->color = RED;
-                    x = x->father;
-                }
-                else {
-                    if (w->left->color == BLACK) {
-                        w->right->color = BLACK;
-                        w->color = RED;
-                        leftRotate(w);
-                        w = x->father->left;
-                    }
-                    w->color = x->father->color;
-                    x->father->color = BLACK;
-                    w->left->color = BLACK;
-                    rightRotate(x->father);
-                    x = header->right;
-                }
-            }
-        }
-        x->color = BLACK; 
-    }
-
-    void setParent(RedBlackNode* x) {
-        if (x == nullNode)
-            return;
-        x->left->father = x;
-        x->right->father = x;
-        setParent(x->left);
-        setParent(x->right);
+        return k2;
     }
 };
 
--- lab3/Basic/Basic.cpp
+++ lab3/Basic/Basic.cpp
@@ -1,19 +1,44 @@
 /*-----------------------------------------------------------------------
-** File Name: Basic1.cpp
+** File Name: Basic.cpp
 ** ID:5130379059
 ** Name: Jin Jianian
-** Created Time: 2014年11月21日 星期五 17时55分03秒
+** Created Time: 2014年11月21日 星期五 17s时55分03秒
 ** ----------------------------------------------------------------------
+** BASIC Interpreter: a very simple one using codes from RPN's homework.
 */
-#include<iostream>
-#include<string>
-#include<map>
-#include<vector>
-#include<stdio.h>
-#include<stdlib.h>
-
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
 using namespace std;
+
+#define KEYNUM 12
+string keywords[KEYNUM] = {"run","clear","quit","list","help","if","goto","then","end","input","print","let"};	
+map<string,int> vals;	/*use to store variables*/
+
+bool IsWrong = false;	/*use to judge whether expression is wrong*/
+bool Jmp_to_begin = false;	/*use to judge meeting a GOTO or THEN*/
+
+/**************some useful functions**************/
+bool IsKeyword(string s){
+/*to judge whether string s is keyword*/
+	for(int i = 0; i < KEYNUM; i++)
+		if(s == keywords[i]) return true;
+	return false;
+}
+bool IsEqual(string s1, string s2){
+/*judge whether s1 is partly same as s2*/
+	if(s2 == "run" ||s2 == "clear" || s2 == "quit" || s2 == "list" || s2 == "help")
+		if(s1.size() != s2.size()) return false;
+	for(int i = 0 ; i < s2.size(); i++){
+		if(s1[i] != s2[i]) return false;
+	}
+	return true;
+}
 string NoSpace(string test){
+/*return string with no space*/
 	string tested = "";
 	for(int i = 0; i < test.size(); i++){
 		if(test[i] != ' ') tested += test[i];
@@ -21,6 +46,7 @@ string NoSpace(string test){
 	return tested;
 }
 string lower(string s){
+/*return string all in lower case*/
 	string temp = s;
 	for(int i = 0; i < s.size();i++){
 		if(s[i]>='A' && s[i] <= 'Z')
@@ -31,11 +57,23 @@ string lower(string s){
 	//cout << endl;
 	return temp;
 }
+bool valid(string s){
+/*judge if INPUT a valid number, '-' for negative one*/
+	for(int i = 0; i < s.size(); i++){
+		if((s[i] < '0' || s[i] > '9') 
+			&& (s[i] != '-' )) 
+			return false;
+	}
+	return true;
+}
+
+/*************** expression tree ******************/
 struct Tree{
 	string var;
 	Tree* Left;
 	Tree* Right;
 	Tree(string s){
+	/*To build a expression tree ---- same to Homework7*/
 		int pos = -1;
 		int times = 0;  
 		bool flag = true;
@@ -76,114 +114,178 @@ struct Tree{
 			var = s[pos];
 		}
 	}
-	void eval(const Tree* root){
-		if(root->left != NULL && root->right != NULL)
-			return root->var;
-		int num1 = eval(root->left);
-		int num2 = eval(root->right);
-		return calculate(num1,num2,root->data);
-	}
-	int calculate(int num,int num2,wchar_t op){
-		int ret = (1<<31);
-		switch(op){
-		case '+':
-			ret = (num + num2);
-			break;
-		case '-':
-			ret = (num - num2);
-			break;
-		case '*':
-			ret = (num * num2);
-			break;
-		case '/':
-			ret = (num/num2);
-			break;
+	int eval(const Tree* root){
+	/*return answer for an expression*/
+		int i;
+		int result;
+		if(root->Left == NULL && root->Right == NULL){
+			if(root->var[0] < '0' || root->var[0] > '9') {
+				IsWrong = false;
+				if(vals.find(lower(root->var)) == vals.end() ){
+					cout << "VARIABLE NOT DEFINED\n"; 
+					IsWrong = true;
+				}
+				else result = vals[lower(root->var)];
+			}else result = atoi(root->var.c_str());
+			return result;
 		}
-		return ret;
+
+		switch(root->var[0]){
+			case '+':
+				return eval(root->Left) + eval(root->Right);
+				break;
+			case '-':
+				return eval(root->Left) - eval(root->Right);
+				break;
+			case '*':
+				return eval(root->Left) * eval(root->Right);
+				break;
+			case '/':
+			{
+				int left = eval(root->Left);
+				int right = eval(root->Right);
+				if(right == 0)  {cout << "DIVIDE BY ZERO\n";right = 1;IsWrong = true;}
+				return left / right;
+				break;
+			}
+		}
 	}
 };
+
+/*************** BASIC Interpreter ****************/
 class Interpreter{
 private:
-	vector<string> comments;
-	map<string,float> vals;
 	map<int,string> codes;
 public:
 	Interpreter(){};
+
 	void execve(){
+	/*use to keep a command line*/
 		string s;
 		while(getline(cin,s)){
 			string n = lower(s);
-			//cout<< n <<endl;
-			if(n == "run") run();
-			else if(n == "clear") clear();
-			else if(n == "quit")  quit();
-			else if(n == "list")  list();
-			else if(n == "help")  help();
-			else judge_if_now(s);
+			if(IsEqual(n,"run")) 	run();	else 
+			if(IsEqual(n,"clear"))	clear();else 
+			if(IsEqual(n,"quit"))  	exit(0);else 
+			if(IsEqual(n,"list"))  	list();	else 
+			if(IsEqual(n,"help"))  	help();	else 
+			if(n == "")				continue;else
+									judge_if_now(s);
 		}
 		return;
 	}
+
 	void clear(){
 		codes.clear();
 		vals.clear();
 	}
+
 	void help(){
-		cout << "Name:Jin Jianian\n" << "No:5130379059\n" << "Merry Christmas :D\n";
+		cout << "Name:Jin Jianian\n" << "No:5130379059\n" << "Don't stay up too late :D\n";
 		return;
 	}
+
 	void list(){
 		for(map<int,string>::iterator i = codes.begin();i != codes.end();i++){
 			if(i->second != "") cout << i->first << " " << i->second << endl;
 		}
 		return;
 	}
-	void quit(){
-		exit(0);
-	}
+
 	int expression(string s){
-		map<string,float>::iterator iter = vals.begin(); 
-		if(s[0] >= 'a' && s[0] <= 'z') {iter = vals.find(s);return (int)iter->second;}
-		else return atoi(s.c_str());
+	/*input a string, return the value of this expression*/
+		string test = NoSpace(s);
+		Tree* rpn = new Tree(test);
+		return rpn->eval(rpn);	
 	}
+
 	bool boolean(string s){
-		return true;
+	/*input a string, return the result of comparation*/
+		int i;
+		for(i = 0; i < s.size(); i++){
+			if(s[i] == '>' || s[i] == '<' || s[i] == '=')  break;
+		}
+		string s1 = NoSpace(s.substr(0,i));  	string s2 = NoSpace(s.substr(i+1));
+		Tree* t1 = new Tree(s1);	        	Tree* t2 = new Tree(s2);
+		int num = t1->eval(t1) - t2->eval(t2);
+		if((num == 0 && s[i] == '=') || (num < 0 && s[i] == '<') || (num > 0 && s[i] == '>'))
+			return true;
+		return false;
 	}
+
 	void handler(string s){
-		int i = 0;string key = "";
-		while(i <= 2) key += s[i++];
-		key = lower(key);
-		if(key == "let"){
+	/*handler OUTSIDE commands*/
+		string test = lower(s);
+		
+		if(IsEqual(test,"let ")){
 			int i;
 			for(i = 4; i < s.size(); i++){
 				if (s[i] == '=') break;
 			}
-			string var_name = s.substr(4,i-4);
-			float var_value = expression(s.substr(i+1));
-			vals[var_name] = var_value;
-		}else if(key == "pri"){
-			string p = s.substr(6);
-			cout << expression(p) << endl;
-		}else if(key == "inp"){
-			string var_name = s.substr(6);
-			float value;
-			cout << "?";
-			cin >> value;
-			vals[var_name] = value;
+			if(i == s.size() - 1) {cout << "SYNTAX ERROR\n";return;}
+			string var_name = lower(NoSpace(s.substr(4,i-4)))	;
+			int var_value = expression(s.substr(i+1));
+			if(IsKeyword(var_name))	{
+				cout << var_name <<": ONE OF KEYWORD \n";
+				return;
+			}
+			else if(var_name[0] >= '0' && var_name[0] <= '9')
+				cout <<"Variable cannot start by a number\n";
+			else{
+				vals[var_name] = var_value;
+			}
 		}else 
-			return;
+		
+		if(IsEqual(test,"print ")){
+			string p = s.substr(6);
+			int ans = expression(p);
+			if(!IsWrong)
+				cout << expression(p) << endl;
+			else return;
+		}else 
+
+		if(IsEqual(test,"input ")){
+			string var_name = s.substr(6);
+			string s;
+			int var_value;
+			cout << " ? ";
+			while(getline(cin,s)){
+				if(valid(s)){
+					var_value = atoi(s.c_str());
+					break;
+				}  
+				cout << "INVALID NUMBER\n ? ";
+			} 	 
+			if(IsKeyword(var_name))	{
+				cout << var_name <<": ONE OF KEYWORD \n";
+				return;
+			}
+			else if(var_name[0] >= '0' && var_name[0] <= '9')
+				cout << var_name <<" cannot start by a number\n";
+			else{
+				vals[var_name] = var_value;
+			}
+		}else
+			cout << "SYNTAX ERROR\n";
+
+		return;
 	}
+
 	void judge_if_now(string s){
+	/*judge whether the command should be store or do it right now*/
 		int num = 0;
 		string n = "", code = "";
 		if(s[0] >= '0' && s[0] <='9'){
 			for(int i = 0; i < s.size(); i++){
 				if(s[i] >= '0' && s[i] <= '9')  n += s[i];
 				else break;
+				//else {
+				//	cout << "SYNTAX ERROR\n";
+				//	return;
+				//}
 			}
 			num = atoi(n.c_str());
-			//cout << num << endl;
 			code = (s.size()!=n.size()) ? s.substr(n.size() + 1):"";
-			//cout << code << endl;
 			codes[num] = code;
 		}
 		else{
@@ -191,61 +293,116 @@ public:
 		}
 		return;
 	}
-	void handler2(map<int,string>::iterator& iter,string s){
-		int i = 0;string key = "";
-		while(i <= 2) key += s[i++];
-		key = lower(key);
-		if(key == "rem"){
-			return;
-		}else if(key == "let"){
+
+	int handler2(map<int,string>::iterator& iter,string s){
+	/*handler INSIDE commands*/
+		string test = lower(s);
+		if(IsEqual(test,"let ")){
 			int i;
 			for(i = 4; i < s.size(); i++){
 				if (s[i] == '=') break;
 			}
-			string var_name = s.substr(4,i-4);
-			float var_value = expression(s.substr(i+1));
-			vals[var_name] = var_value;
-		}else if(key == "pri"){
-			string p = s.substr(6);
-			cout << expression(p) << endl;
-		}else if(key == "inp"){
+			if(i == s.size() - 1) {cout << "SYNTAX ERROR\n";return 1;}
+			string var_name = lower(NoSpace(s.substr(4,i-4)));
+			int var_value = expression(s.substr(i+1));
+			if(IsKeyword(var_name))	{
+				cout << var_name <<": ONE OF KEYWORD \n";
+				return 1;
+			}
+			else if(var_name[0] >= '0' && var_name[0] <= '9')
+				cout << var_name <<" cannot start by a number\n";
+			else{
+				vals[var_name] = var_value;
+			}
+		}else 
+
+		if(IsEqual(test,"print ")){
+			string p = s.substr(5);
+			int ans = expression(p);
+			if(!IsWrong)
+				cout << ans << endl;
+		}else 
+
+		if(IsEqual(test,"input ")){
 			string var_name = s.substr(6);
-			float value;
-			cout <<"?";
-			cin >> value;
-			vals[var_name] = value;
-		}else if(key == "got"){
+			string s;
+			int var_value;
+			cout << " ? ";
+			while(getline(cin,s)){
+				if(valid(s)){
+					var_value = atoi(s.c_str());
+					break;
+				}  
+				cout << "INVALID NUMBER\n ? ";
+			} 	 
+			if(IsKeyword(var_name))	{
+				cout << var_name <<": ONE OF KEYWORD \n";
+				return 1;
+			}
+			else if(var_name[0] >= '0' && var_name[0] <= '9')
+				cout << var_name <<" cannot start by a number\n";
+			else{
+				vals[var_name] = var_value;
+			}
+		}else 
+
+		if(IsEqual(test,"goto ")){
 			string point = s.substr(5);
 			int n = atoi(point.c_str());
-			cout <<"GOTO:" << n <<endl;
 			iter = codes.find(n);
+			if(iter == codes.end()) {
+				cout << "LINE NUMBER ERROR\n";
+				return 1;
+			}
+			if(iter == codes.begin())	return 2;
 			iter--;
-			return;
-		}else if(key == "if "){
+		}else 
+
+		if(IsEqual(test,"if ")){
 			int i;
 			for(i = 2; i < s.size()-4 ; i++){
 				int j = i;string key = "";
 				while(j <= i+3) key += s[j++];
-				if(key == "then")
+				if(key == "THEN")
 					break;
 			}
-			string ocodes = s.substr(i+5);
-			bool flag = boolean(s.substr(3,i-3));
-			if(flag) handler2(iter,ocodes);
-			return;
-		}else if(key == "end") return;
-		return;//wrong
-	
-	}
-	void run(){
-		//get codes from MAP-codes;
-		map<int,string>::iterator iter = codes.begin();
-		for(;iter != codes.end(); iter++){
-			handler2(iter,iter->second);
+
+			string ocodes = s.substr(i+4);
+			bool flag = boolean(s.substr(2,i-2));
+			if(flag) {
+				iter = codes.find(atoi(ocodes.c_str()));
+				if(iter == codes.end()) {
+					cout << "LINE NUMBER ERROR\n";
+					return 1;
+				}
+				if(iter == codes.begin())	return 2;
+				iter--;
+			}	
+		}else 
+
+		if(IsEqual(test,"end")) return 1;
+		else if(IsEqual(test, "rem")) return 0;
+		else {
+			cout << "SYNTAX ERROR\n";
+			return 1;
 		}
-		//handler right now;
+		return 0;
+	}
+
+	void run(){
+	/*run INSIDE commands*/
+		map<int,string>::iterator iter = codes.begin();
+		while(iter != codes.end()){
+			int status = handler2(iter,(iter)->second);
+			iter++;
+			if(status == 1)	break;
+			if(status == 2) iter--;
+			
+		}
+		//list();
 	}
 };
+
 int main(){
 	Interpreter i;
 	i.execve();
